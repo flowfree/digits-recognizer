b083dbcaaa99eeea5b260dd226cf2d40659d7349
Nash <nashruddin.amin@gmail.com>
2021-09-20 19:51:55 +0700

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-9">
+          <h1 className="text-center">Digits Recognition</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
