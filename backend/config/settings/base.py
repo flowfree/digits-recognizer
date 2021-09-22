5587d23ea374244aa029197428f2673426929f4e
Nash <nashruddin.amin@gmail.com>
2021-09-22 18:25:29 +0700

    Allow CORS from localhost

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -99,6 +99,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost',
     'http://localhost:3000',
     'http://localhost:8000',
 ]
