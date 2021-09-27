90d09fa3500ad2fd59c0535b2a9f66addb75b136
Nash <nashruddin.amin@gmail.com>
2021-09-27 10:52:26 +0700

    Added loading message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import { useEffect, useState } from 'react'
 import DigitsRecognizer from './components/DigitsRecognizer'
 
 // Get the base URL for the backend
 // Note that you can set this variable in the `.env` file.
 const baseURL = process.env.REACT_APP_API_BASE_URL 
 
+axiosRetry(axios, { retries: 3 })
+
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    console.log(baseURL)
+    axios 
+      .get(`${baseURL}/`)
+      .then(() => {
+        setIsLoaded(true)
+        setError('')
+      })
+      .catch(() => {
+        setIsLoaded(false)
+        setError('Something went wrong.')
+      })
+  }, [])
+
   return (
     <div className="container">
       <div className="row text-center justify-content-center">
@@ -13,7 +34,14 @@ function App() {
           <h1 className="mt-5 mb-3">Digits Recognizer</h1>
         </div>
       </div>
-      <DigitsRecognizer />
+      {isLoaded ? (
+        <DigitsRecognizer />
+      ) : (
+        <p className="text-center">Please wait while we're warming up the AI code...</p>
+      )}
+      {error && (
+        <div className="text-center text-danger">{error}</div>
+      )}
     </div>
   )
 }
